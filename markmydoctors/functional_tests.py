ce77c98c6bdab48ba5e49c09ec14c34bddf44e5f
viviruska <vivien.ruska@prezi.com>
2020-02-18 00:04:45 +0100

    test: Functional test now checks we can input a doctor

--- markmydoctors/functional_tests.py
+++ markmydoctors/functional_tests.py
@@ -1,5 +1,7 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -18,17 +20,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mentions Mark my doctors
         self.assertIn('Mark my doctors', self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('Mark my doctors', header_text)
 
         # She is invited to enter a doctor's name straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            "Not the doctor you're looking for? Add him/her here"
+        )
 
         # She types "Ruska Pal" into a text box
+        inputbox.send_keys('Ruska Pal')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Ruska Pal" as a doctor in a rating list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Ruska Pal' for row in rows)
+        )
 
         # There is still a text box inviting her to add another doctor. She
         # enters "Irisz Szabo"
+        self.fail("Finish the test!")
 
         # The page updates again, and now shows both doctors on her list
 
