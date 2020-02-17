eb3c61a2a9575fc76e50b3934b3070f14e4c6dab
viviruska <vivien.ruska@prezi.com>
2020-02-17 22:03:41 +0100

    Basic view now returns minimal HTML

--- markmydoctors/lists/tests.py
+++ markmydoctors/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.urls import resolve
 from django.test import TestCase
 from .views import home_page
@@ -8,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_homepage_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  # this is what Django will see when a user's browser asks for a page
+        response = home_page(request)  # instance of a class called HttpResponse
+        html = response.content.decode('utf-8')  # to convert raw bytes into the string of HTML
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Mark my doctors</title>', html)
+        self.assertTrue(html.endswith('</html>'))
