64e9a89cc3f32e386fd3c6229096b583f73d834f
viviruska <vivien.ruska@prezi.com>
2021-01-28 10:06:41 +0100

    fix: remove line from settings

--- markmydoctors/markmydoctors/settings.py
+++ markmydoctors/markmydoctors/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gm3f*rxbe836#n+!4ez0yg^uqximc#lj@5lxe()ggl*u&^i-d0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
